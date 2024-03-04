6c97d2b8a2431ecbae5d6977cfe655a351a59fd4
SemiTone <alexeyton@mail.ru>
2024-03-04 23:08:19 +0500

    Переписал  с использованием mathAll

--- 4-timer/app.js
+++ 4-timer/app.js
@@ -1,47 +1,63 @@
-//Чтобы как-то роазнообразить урок я решил сделать с регулярными выражениеми
 const args = process.argv.slice(2).join('')
 const search = /\d+([hms]|[HMS]|[чсм]|[ЧСМ]|([Hh]our)|([Mm]inute)|([Ss]econd)|([Чч]ас)|([Сс]екунд)|([Мм]инут))/g
 
 const match = {
-    hour: RegExp('\\d+([HhЧч]|([Hh]our)|([Чч]ас))', "g"),
-    min: RegExp('\\d+([MmМм]|([Mm]inute)|([Мм]инут))', "g"),
-    sec: RegExp('\\d+([SsСс]|([Ss]econd)|([Сс]екунд))', "g")
+    hour: RegExp('([HhЧч]|([Hh]our)|([Чч]ас))', "g"),
+    min: RegExp('([MmМм]|([Mm]inute)|([Мм]инут))', "g"),
+    sec: RegExp('([SsСс]|([Ss]econd)|([Сс]екунд))', "g")
+}
+
+const TIME = {
+    hour: 3_600_000,
+    min: 60_000,
+    sec: 1_000
 }
 
 let totalTime = 0;
-let hour = 0, min = 0, sec = 0;
+
+function getTime(totalTime) {
+    let remainder = totalTime;
+
+    const hour = Math.floor(remainder / TIME.hour);
+    remainder = totalTime % TIME.hour;
+
+    const min = Math.floor(remainder / TIME.min);
+    remainder = totalTime % TIME.min;
+
+    const sec = Math.floor(remainder / TIME.sec);
+    remainder = totalTime % TIME.sec;
+
+    return {hour, min, sec};
+}
 
 function main() {
-    const timeArgs = args.match(search)
+
+    const timeArgs = Array.from(args.matchAll(search));
+
     if (!timeArgs) {
-        console.log(' Не правильно введены аргументы!');
-        return;
+        throw new Error('Не правильно введены аргументы!')
     } 
 
     timeArgs.forEach(el => {
-
-        if (el.match(match.hour)) {
-            const count = Number.parseInt(el.match(match.hour))
+        if (el[1].match(match.hour)) {
+            const count = Number.parseInt(el[0])
             totalTime += count * 60 * 60 * 1000;
-            hour = count
         }
-        else if (el.match(match.min)) {
-            const count = Number.parseInt(el.match(match.min))
+        else if (el[1].match(match.min)) {
+            const count = Number.parseInt(el[0])
             totalTime += count * 60 * 1000;
-            min = count
         }
-        else if (el.match(match.sec)) {
-            const count = Number.parseInt(el.match(match.sec))
+        else if (el[1].match(match.sec)) {
+            const count = Number.parseInt(el[0])
             totalTime += count * 1000;
-            sec = count
         }
     });
-    
-    
+
     setTimeout(() => {
         console.log('Время вышло!')
     }, totalTime)
 
+    const {hour, min, sec} = getTime(totalTime);
     console.log(`Установлен таймер на ${hour} часов ${min} минут ${sec} секунд`)
  }
 
